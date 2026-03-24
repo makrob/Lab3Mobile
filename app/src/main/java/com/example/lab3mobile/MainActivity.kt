360bda73b8ce47780f21d10a3d69cc5649d25ffd
Daniil Prokofyev <webden.mak@yandex.ru>
2026-03-25 01:16:33 +0400

    feat(tips): implement main tips list functionality

--- app/src/main/java/com/example/lab3mobile/MainActivity.kt
+++ app/src/main/java/com/example/lab3mobile/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.lab3mobile
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab3mobile.adapter.TipAdapter
+import com.example.lab3mobile.data.TipsData
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var toolbar: MaterialToolbar
+    private var currentLayoutManager = TipAdapter.LAYOUT_MANAGER_VERTICAL
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.app_name)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView)
+        val tips = TipsData.getTips()
+        val adapter = TipAdapter(tips, currentLayoutManager)
+
+        when (currentLayoutManager) {
+            TipAdapter.LAYOUT_MANAGER_VERTICAL -> {
+                recyclerView.layoutManager = LinearLayoutManager(this)
+            }
+            TipAdapter.LAYOUT_MANAGER_HORIZONTAL -> {
+                recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            }
+            TipAdapter.LAYOUT_MANAGER_GRID -> {
+                recyclerView.layoutManager = GridLayoutManager(this, 2)
+            }
         }
+
+        recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/lab3mobile/model/Tip.kt
@@ -0,0 +1,9 @@
+package com.example.lab3mobile.model
+
+data class Tip(
+    val id: Int,
+    val day: Int,
+    val title: String,
+    val description: String,
+    val imageResId: Int
+)
